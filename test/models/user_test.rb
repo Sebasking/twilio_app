ffbcedabc9ed2fc7ff154e8dc4a1549ab8fb0682
Sebastian King <sebasking@gmail.com>
2023-11-28 10:42:32 -0500

    add user and messages model

---
+++ test/models/user_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "does not sign in the user with out a valid email" do
+    
+  end
+end
