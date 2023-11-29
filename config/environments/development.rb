93cb1a2fe8a49808c5c940276b7b58cee9b80d37
Sebastian King <sebasking@gmail.com>
2023-11-29 15:20:06 -0500

    fix test fixtures to have unique emails to pass uniqueness constraint

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.hosts << "killthedj.ngrok.io"
+  config.hosts << "killthedj.ngrok.io"
 end
