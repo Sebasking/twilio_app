ce5615919bf2d77c00b0ac1509965f523a7e228e
Sebastian King <sebasking@gmail.com>
2023-12-02 12:35:49 -0500

    debug

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,16 @@
 class MessagesController < ApplicationController
     before_action :authenticate_user!
 
-    def show
-        puts 'in the show'
-    end
-
     def index
-        puts 'we all up in here'
         @messages = Message.where(user_id: current_user.id)
         render json: @messages
     end
 
     def create 
-
+        puts 'did i make it here'
+        puts params
+        result = TwilioClient.send_message(from: message_params['from'], to: message['to'])
+        puts result
     end
 
     private
