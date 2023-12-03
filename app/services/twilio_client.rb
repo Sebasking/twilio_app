48ec67f6e255c5b1ee5dd7d2aec9a0de464c1ddc
Sebastian King <sebasking@gmail.com>
2023-12-03 16:58:34 -0500

    delete old files

--- app/services/twilio_client.rb
+++ app/services/twilio_client.rb
@@ -4,19 +4,13 @@ class TwilioClient
     @@client = Twilio::REST::Client.new(account_sid, auth_token)
 
     def self.send_message(to, body)
-        puts 'did we make it here'
         begin 
-            result = @@client.messages.create(
-                from: ENV['TWILIO_NUMBER'],
-                to: to,
+            @@client.messages.create(
+                from: "+#{ENV['TWILIO_NUMBER']}",
+                to: "+#{to}",
                 body: body
             )
-
-            puts result
-            result
         rescue Twilio::REST::RestError => e
-            puts e
-            puts 'It fucking failed'
             e
         end
     end
