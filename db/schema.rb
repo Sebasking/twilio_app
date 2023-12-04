5cf9293d1ed5bba00783bc42714e958cc9722ff0
Sebastian King <sebasking@gmail.com>
2023-12-04 09:21:46 -0500

    switch to postgres to match fly.io

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_02_213427) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "body"
     t.string "to"
     t.datetime "created_at", null: false
